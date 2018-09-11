bd2b6c686157a9ac72bc1881b12decd71bc861b5
LayneKM <laynemikan@gmail.com>
2018-09-11 17:12:34 -0400

    HandTest: created test cases for Hand class

---
+++ src/test/java/blackjack/HandTest.java
@@ -0,0 +1,28 @@
+package blackjack;
+
+import junit.framework.TestCase;
+
+public class HandTest extends TestCase{
+
+	public void testGetTotalWithHighAce() {
+		Hand testerHand = new Hand();
+		Card card1 = new Card("D", "10");
+		Card card2 = new Card("S", "A");
+		testerHand.addCard(card1);
+		testerHand.addCard(card2);
+		
+		assertEquals(21, testerHand.getTotal());
+	}
+	
+	public void testGetTotalWithLowAce() {
+		Hand testerHand = new Hand();
+		Card card1 = new Card("C", "2");
+		Card card2 = new Card("D", "10");
+		Card card3 = new Card("S", "A");
+		testerHand.addCard(card1);
+		testerHand.addCard(card2);
+		testerHand.addCard(card3);
+		
+		assertEquals(13, testerHand.getTotal());
+	}
+}
