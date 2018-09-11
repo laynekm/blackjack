f9ce132bd1298e35204594a5b22c82c02bc108c7
LayneKM <laynemikan@gmail.com>
2018-09-11 18:00:53 -0400

    PlayerTest: created test cases for Player class

---
+++ src/test/java/blackjack/PlayerTest.java
@@ -0,0 +1,15 @@
+package blackjack;
+
+public class PlayerTest {
+	public void testDisplayCards() {
+		Player player = new Player();
+		Card card1 = new Card("D", "2");
+		Card card2 = new Card("S", "A");
+		Card card3 = new Card("H", "K");
+		player.getHand().addCard(card1);
+		player.getHand().addCard(card2);
+		player.getHand().addCard(card3);
+		
+		assertEquals("[D2] [SA] [HK]", player.getCardString());
+	}
+}
