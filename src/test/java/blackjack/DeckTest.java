91758832d81b699c8e273d505a4dc4943c1efc84
LayneKM <laynemikan@gmail.com>
2018-09-11 16:45:53 -0400

    DeckTest: refactored Deck test cases

--- src/test/java/blackjack/DeckTest.java
+++ src/test/java/blackjack/DeckTest.java
@@ -5,29 +5,49 @@ import java.util.Arrays;
 import junit.framework.TestCase;
 
 public class DeckTest extends TestCase{
+	public void testConstructor() {
+		Deck deck = new Deck();
+		
+		//test that deck is correct length
+		assertTrue(deck.getCards().length == 52);
+		
+		//test that deck contains all correct suits/ranks
+		//- just look at output for now
+	}
+	
 	public void testShuffle() {
 		Deck deck = new Deck();
-		String[] cards = deck.getCards();
-		deck.shuffle();
-		String[] shuffledCards = deck.getCards();
+		Card[] cards = deck.getCards();
 		
-		//test shuffled deck != original deck
-		assertFalse(Arrays.equals(cards, shuffledCards));
+		deck.shuffle();
+		Card[] shuffledCards = deck.getCards();
+		
+		//test that shuffled deck is still the correct length
+		assertTrue(deck.getCards().length == 52);
+		
+		//test shuffled deck != original deck (ie. at least one card is different)
+		int equalChecker = 1; 
+		for(int i = 0; i < cards.length; i++) {
+			if(cards[i].toString() != shuffledCards[i].toString()) {
+				equalChecker = 0;
+			}
+		}
+		assertTrue(equalChecker == 0);
 	}
 	
 	public void testDealCard() {
 		Deck deck = new Deck();
-		String[] cards = deck.getCards();
-		String dealtCard = deck.dealCard();
-		String[] newCards = deck.getCards();
+		Card[] cards = deck.getCards();
+		Card dealtCard = deck.dealCard();
+		Card[] newCards = deck.getCards();
 		
 		//test that dealtCard is first in deck
-		assertEquals(cards[0], dealtCard);
+		assertEquals(cards[0].toString(), dealtCard.toString());
 		
 		//test that dealtCard is no longer in deck
-		assertFalse(Arrays.asList(newCards).contains(dealtCard));
+		assertFalse(Arrays.asList(newCards.toString()).contains(dealtCard.toString()));
 		
 		//test that new deck is one smaller than original
-		assertEquals(newCards, cards.length - 1);
+		assertEquals(newCards.length, cards.length - 1);
 	}
 }
