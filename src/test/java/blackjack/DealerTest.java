f0912f4f40c86b30bab18edc231aebca800c0bd9
LayneKM <laynemikan@gmail.com>
2018-09-11 18:05:51 -0400

    DealerTest: created test cases for Dealer class

---
+++ src/test/java/blackjack/DealerTest.java
@@ -0,0 +1,25 @@
+package blackjack;
+
+import junit.framework.TestCase;
+
+public class DealerTest extends TestCase{
+	public void testGetCardStringVisible() {
+		Dealer player = new Dealer();
+		Card card1 = new Card("D", "2");
+		Card card2 = new Card("S", "A");
+		player.getHand().addCard(card1);
+		player.getHand().addCard(card2);
+		
+		assertEquals("[D2] [SA]", player.getCardStringVisible());
+	}
+	
+	public void testGetCardStringHidden() {
+		Dealer player = new Dealer();
+		Card card1 = new Card("D", "2");
+		Card card2 = new Card("S", "A");
+		player.getHand().addCard(card1);
+		player.getHand().addCard(card2);
+		
+		assertEquals("[D2] [||]", player.getCardStringVisible());
+	}
+}
