7429cfa193dc2a125804d01f4f87ee6bda239829
LayneKM <laynemikan@gmail.com>
2018-09-13 11:51:30 -0400

    GameControllerTest: fixed test cases

--- src/test/java/blackjack/GameControllerTest.java
+++ src/test/java/blackjack/GameControllerTest.java
@@ -6,12 +6,14 @@ import static org.junit.Assert.*;
 import java.io.File;
 
 public class GameControllerTest extends TestCase{
-	public void promptForInputTypeTest() {
+	public void isValidTypeInput() {
 		GameController game = new GameController();
-		String inputType = game.promptForInputType();
 		
 		//verify inputType can only be "C" or "F"
-		assertTrue(inputType == "C" || inputType == "F");
+		assertTrue(game.isValidTypeInput("C"));
+		assertTrue(game.isValidTypeInput("F"));
+		assertFalse(game.isValidTypeInput("X"));
+		assertFalse(game.isValidTypeInput(""));
 	}
 	
 	public void isValidFileTest() {
@@ -38,6 +40,25 @@ public class GameControllerTest extends TestCase{
 		
 		//verify actualArray = expectedArray
 		assertArrayEquals(expectedArray, actualArray);
+		
+	}
+	
+	public void playWithFileInputTest() {
+		GameController game = new GameController();
+		
+		//test case where dealer wins
+		File file1 = new File("src/main/resources/inputFile1.txt");
+		String[] fileArray1 = game.readFileToArray(file1);
+		assertEquals("dealer", game.playWithFileInput(fileArray1));
+		
+		//test case where player wins
+		File file2 = new File("src/main/resources/inputFile2.txt");
+		String[] fileArray2 = game.readFileToArray(file2);
+		assertEquals("player", game.playWithFileInput(fileArray2));
+	}
+	
+	//no idea how this is supposed to work
+	public void playWithConsoleInputTest() {
 		
 	}
 }
